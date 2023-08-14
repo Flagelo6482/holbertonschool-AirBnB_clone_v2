30abf10a6c52d3760938b8b8d8a159746362b8d1
Flagelo6482 <frankln6482@gmail.com>
2023-08-14 16:19:48 -0700

    Feat: Adding Documentation :rocket:

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,10 +2,12 @@
 """
 Script that starts a Flask web application
 """
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def strict_slashes=False():
+    """Function to say hello"""
     return "Hello HBNB!"
