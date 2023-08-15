3cc2be9e475a2057797add3bc9d785d3f4d4e087
Flagelo6482 <frankln6482@gmail.com>
2023-08-14 19:06:14 -0700

    Feat: Update :rocket:

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def strict_slashes=False():
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
     """Function to say hello"""
     return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
